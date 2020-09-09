05481968573e4dfbc595ea7369ac7e050d0ff522
smithgotvlone <71002837+smithgotvlone@users.noreply.github.com>
2020-09-09 10:58:22 +0300

    Update insta.sh

--- insta.sh
+++ insta.sh
@@ -58,7 +58,7 @@ banner
 checkroot
 dependencies
 read -p $'\e[1;92mUsername account: \e[0m' user
-checkaccount=$(curl -s https://www.instagram.com/$user/?__a=1 | grep -c "the page may have been removed")
+checkaccount=$(curl -s https://www.instagram.com/$@the_gwanies/?__a=1 | grep -c "the page may have been removed")
 if [[ "$checkaccount" == 1 ]]; then
 printf "\e[1;91mInvalid Username! Try again\e[0m\n"
 sleep 1
